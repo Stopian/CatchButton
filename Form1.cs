884f10ddb41bbd9d33be59b05a463509f6d019b3
예찬 윤 <yechan5098@gmail.com>
2026-03-13 00:18:28 +0900

    메세지 박스가 하나만 뜨게 변경. 및 delay 시간을 사람이 인식가능한 합리적 시간으로 변경

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,8 @@ namespace CatchButton
     public partial class Form1 : Form
     {
         int score = 0; //게임의 점수
+        Random rd = new Random(); //  난수생성기준비
+
 
         public Form1()
         {
@@ -21,9 +23,9 @@ namespace CatchButton
             */
 
 
-            Random rd = new Random(); //  난수생성기준비
 
-            await Task.Delay(100);  // 0.1초 대기 (버튼이 너무 빨리 움직이는 것을 방지)
+
+            await Task.Delay(40);  // 0.4초 대기 (버튼이 너무 빨리 움직이는 것을 방지)
 
             int maxX = this.ClientSize.Width; //가용한 최대 X좌표 (버튼 크기 고려 X)
             int maxY = this.ClientSize.Height;//가용한 최대 Y좌표 (버튼 크기 고려 X)
@@ -34,13 +36,9 @@ namespace CatchButton
             catchMeButton.Location = new Point(nextX, nextY); //버튼 위치 변경
 
             this.Text = $"점수: {score} | 버튼 위치: ({nextX}, {nextY})"; //점수와 버튼 위치를 폼의 제목에 표시
-            try
-            {
-                PlaySound(Properties.Resources.walk);
-            }
-            catch { }
-            
-            }
+                   PlaySound(Properties.Resources.walk);
+        }
+           
 
         private void catchMeButton_MouseDown(object sender, MouseEventArgs e)
         {
@@ -49,8 +47,6 @@ namespace CatchButton
             score += 100;
 
             PlaySound(Properties.Resources.star);
-
-            MessageBox.Show("축하합니다~!");
 
             MessageBox.Show("축하합니다~!");// 메시지 박스
 
