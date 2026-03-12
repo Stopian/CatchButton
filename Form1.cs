564bd53bd022ebc53b7c9902e6d41aaaebf92af6
예찬 윤 <yechan5098@gmail.com>
2026-03-13 00:54:00 +0900

    버튼의 크기가 기존의 고정된 값이 아니고 변동이 되기 때문에 'max - catchMeButton.~~' 로 수정함.
    버튼을 클릭하는데 실패하면 점수가 10점 깎이도록 코드를 구성함.
    
    클릭에 성공할 시 버튼의 크기를 10%줄이고 폰트 또한 10% 줄이도록 코드를 구성함.

--- Form1.cs
+++ Form1.cs
@@ -5,7 +5,8 @@ namespace CatchButton
     public partial class Form1 : Form
     {
         int score = 0; //게임의 점수
-        Random rd = new Random(); //  난수생성기준비
+        int MissCount = 0;  //놓친 횟수
+        Random rd = new Random(); //  난수생성기준
 
 
         public Form1()
@@ -15,28 +16,25 @@ namespace CatchButton
 
         private async void catchMeButton_MouseEnter(object sender, EventArgs e)
         {
-            /*
-            int x_position = 100;
-            int y_position = 150;
 
-            catchMeButton.Location = new Point(x_position, y_position);
-            */
-
-
-
+            if(score>0)
+            {
+                   score -= 10; //점수 감소
+            }
 
             await Task.Delay(40);  // 0.4초 대기 (버튼이 너무 빨리 움직이는 것을 방지)
 
             int maxX = this.ClientSize.Width; //가용한 최대 X좌표 (버튼 크기 고려 X)
             int maxY = this.ClientSize.Height;//가용한 최대 Y좌표 (버튼 크기 고려 X)
 
-            int nextX = rd.Next(0, maxX - 267); // 0부터 maxX-1까지의 난수 생성 (버튼의 너비 267 고려)
-            int nextY = rd.Next(0, maxY - 91); // 0부터 maxY-1까지의 난수 생성 (버튼의 높이 91 고려)
+            int nextX = rd.Next(0, maxX - catchMeButton.Width); // 0부터 maxX-1까지의 난수 생성 (버튼의 너비 고려)
+            int nextY = rd.Next(0, maxY - catchMeButton.Height); // 0부터 maxY-1까지의 난수 생성 (버튼의 높이 고려)
 
             catchMeButton.Location = new Point(nextX, nextY); //버튼 위치 변경
 
             this.Text = $"점수: {score} | 버튼 위치: ({nextX}, {nextY})"; //점수와 버튼 위치를 폼의 제목에 표시
                    PlaySound(Properties.Resources.walk);
+           
         }
            
 
@@ -49,7 +47,11 @@ namespace CatchButton
             PlaySound(Properties.Resources.star);
 
             MessageBox.Show("축하합니다~!");// 메시지 박스
-
+            catchMeButton.Width = (int)(catchMeButton.Width * 0.9); //버튼의 너비를 10% 줄임   
+            catchMeButton.Height = (int)(catchMeButton.Height * 0.9); //버튼의 높이를 10% 줄임
+            float FontNewSize = catchMeButton.Font.Size * 0.9f;
+            catchMeButton.Font = new Font(catchMeButton.Font.FontFamily, 
+                FontNewSize, catchMeButton.Font.Style); //버튼의 폰트 크기를 10% 줄임
         }
         private void PlaySound(byte[] soundData) // byte 배열로부터 사운드를 재생하는 메서드
         {
@@ -59,5 +61,6 @@ namespace CatchButton
                 sp.Play();
             }
         }
+        
     }
 }
