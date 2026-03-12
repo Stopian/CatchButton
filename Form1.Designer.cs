731352386ba900adab0802e91a956a4c9bc4e95a
기주 이 <gijulee012@gmail.com>
2026-03-12 12:11:28 +0900

    버튼 이름 catchMeButton 으로 수정

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            catchMeButton = new Button();
+            SuspendLayout();
+            // 
+            // catchMeButton
+            // 
+            catchMeButton.BackColor = SystemColors.ActiveCaption;
+            catchMeButton.Font = new Font("맑은 고딕", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            catchMeButton.Location = new Point(264, 162);
+            catchMeButton.Name = "catchMeButton";
+            catchMeButton.Size = new Size(267, 91);
+            catchMeButton.TabIndex = 0;
+            catchMeButton.Text = "나를 잡아봐";
+            catchMeButton.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(catchMeButton);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button catchMeButton;
     }
 }
