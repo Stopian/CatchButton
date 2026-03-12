f8ccfd764c6ef8ce5cf4045525b1e9aa099eeb2d
예찬 윤 <yechan5098@gmail.com>
2026-03-12 23:34:26 +0900

    점수와 버튼위치가 폼의 제목에 정상적으로 출력되지 않아 이를 해결함. 기존에 효과음의 동작에서 발생하던 오류를 System.IO 및 System.Media 를 통해 해결함.
    버튼이 너무 빨리 움직여 난이도가 불가능에 가깝기에 Task.Delay 를 통해 너무 빠르게 움직이는 것을 방지

--- Form1.cs
+++ Form1.cs
@@ -1,15 +1,17 @@
 using System.Media;
+using System.IO;
 namespace CatchButton
 {
     public partial class Form1 : Form
     {
         int score = 0; //게임의 점수
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void catchMeButton_MouseEnter(object sender, EventArgs e)
+        private async void catchMeButton_MouseEnter(object sender, EventArgs e)
         {
             /*
             int x_position = 100;
@@ -21,6 +23,8 @@ namespace CatchButton
 
             Random rd = new Random(); //  난수생성기준비
 
+            await Task.Delay(100);  // 0.1초 대기 (버튼이 너무 빨리 움직이는 것을 방지)
+
             int maxX = this.ClientSize.Width; //가용한 최대 X좌표 (버튼 크기 고려 X)
             int maxY = this.ClientSize.Height;//가용한 최대 Y좌표 (버튼 크기 고려 X)
 
@@ -29,29 +33,35 @@ namespace CatchButton
 
             catchMeButton.Location = new Point(nextX, nextY); //버튼 위치 변경
 
-            this.Text = $"버튼위치: ({nextX}, {nextY})"; // 폼의 제목에 버튼의 위치 표시
-
-            this.Text = "점수 : " + score; // 폼의 제목에 게임 점수 표시
-
-            /*SoundPlayer player = new SoundPlayer("escape.mp3"); // 버튼이 움직일 때 효과음
-            player.Play();
-            */
-        }
+            this.Text = $"점수: {score} | 버튼 위치: ({nextX}, {nextY})"; //점수와 버튼 위치를 폼의 제목에 표시
+            try
+            {
+                PlaySound(Properties.Resources.walk);
+            }
+            catch { }
+            
+            }
 
         private void catchMeButton_MouseDown(object sender, MouseEventArgs e)
         {
-            /*
-            SoundPlayer player = new SoundPlayer("success.mp3");// 잡았을 때 효과음
-            player.Play();*/
+            SystemSounds.Asterisk.Play();
 
             score += 100;
 
-            this.Text = "점수 : " + score;
+            PlaySound(Properties.Resources.star);
 
             MessageBox.Show("축하합니다~!");
 
             MessageBox.Show("축하합니다~!");// 메시지 박스
 
+        }
+        private void PlaySound(byte[] soundData) // byte 배열로부터 사운드를 재생하는 메서드
+        {
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream(soundData))
+            {
+                System.Media.SoundPlayer sp = new System.Media.SoundPlayer(ms);
+                sp.Play();
+            }
         }
     }
 }
